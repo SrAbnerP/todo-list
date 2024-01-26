4c120adf436a7920f9d425a4d02c577ae8b60cc7
SrAbnerP <abner.sol.pereira@gmail.com>
2024-01-26 20:59:05 -0300

    add the function of delete the todo

--- src/App.jsx
+++ src/App.jsx
@@ -41,12 +41,21 @@ function App() {
     setTodos(newTodos);
   };
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTdodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+
+    setTodos(filteredTdodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
         ))}
       </div>
 
